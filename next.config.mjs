cd1533e32e50b8607e63aafb2808ce0b95cd41ea
Prakhar Tripathi <64783317+prahacker@users.noreply.github.com>
2025-02-07 14:53:50 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'], // Add the external image domain
+      domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com', 'profileimagesfor.s3.ap-south-1.amazonaws.com'], 
     },
   }
   
